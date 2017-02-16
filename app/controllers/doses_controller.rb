50c47530b01f96feea78e8d82de1819153678b3b
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-16 15:19:34 +0000

    done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,22 +1,35 @@
 class DosesController < ApplicationController
+
+
+  before_action :set_cocktail, only: [:new, :create]
+
   def new
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
+    @dose = @cocktail.doses.build
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
   end
 
-  def edit
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
-  def index
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216112936) do
+ActiveRecord::Schema.define(version: 20170216120358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
