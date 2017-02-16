ec2a1a10801d186d47d34d4f91e8761e8629e285
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-16 12:18:21 +0000

    done valdation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/show'
+
+  get 'doses/update'
+
+  get 'doses/destroy'
+
+  get 'doses/create'
+
+  get 'doses/edit'
+
+  get 'doses/index'
+
+  get 'ingredients/new'
+
+  get 'ingredients/show'
+
+  get 'ingredients/update'
+
+  get 'ingredients/destroy'
+
+  get 'ingredients/create'
+
+  get 'ingredients/edit'
+
+  get 'ingredients/index'
+
+  get 'cocktails/new'
+
+  get 'cocktails/show'
+
+  get 'cocktails/update'
+
+  get 'cocktails/destroy'
+
+  get 'cocktails/create'
+
+  get 'cocktails/edit'
+
+  get 'cocktails/index'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
